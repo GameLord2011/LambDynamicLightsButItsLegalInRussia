3906fcae4f03a0b31a2db31c6195473ad0e2f743
Thomas Glasser <thomas@thomasglasser.dev>
2025-03-17 17:50:46 -0400

    Added license headers

--- api/src/main/java/dev/lambdaurora/lambdynlights/api/data/LightSourceProvider.java
+++ api/src/main/java/dev/lambdaurora/lambdynlights/api/data/LightSourceProvider.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright © 2025 LambdAurora <email@lambdaurora.dev>
+ *
+ * This file is part of LambDynamicLights.
+ *
+ * Licensed under the Lambda License. For more information,
+ * see the LICENSE file.
+ */
+
 package dev.lambdaurora.lambdynlights.api.data;
 
 import com.mojang.serialization.Codec;
