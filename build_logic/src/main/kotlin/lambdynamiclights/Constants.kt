a006800dbfb67a8ab216359dcba7c8de890acc30
LambdAurora <email@lambdaurora.dev>
2025-08-25 23:52:01 +0200

    Fix some crashes related to debug renderers.

--- build_logic/src/main/kotlin/lambdynamiclights/Constants.kt
+++ build_logic/src/main/kotlin/lambdynamiclights/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
 	const val NAME = "lambdynamiclights"
 	const val NAMESPACE = "lambdynlights"
 	const val PRETTY_NAME = "LambDynamicLights"
-	const val VERSION = "4.3.2"
+	const val VERSION = "4.3.3"
 	const val JAVA_VERSION = 21
 
 	const val DESCRIPTION = "The most feature-complete dynamic lighting mod for Fabric."
